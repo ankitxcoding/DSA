2848df6214b068886fcfe2075b985d5ad915aecc
ankitxcoding <ankitxcoding@gmail.com>
2022-11-19 20:28:11 +0530

    First and Last Occurrence of array element

---
+++ Day 013 Codes/firstAndLastOccurrence.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+using namespace std;
+
+int firstOccurrence(int arr[], int x, int size);
+int lastOccurrence(int arr[], int x, int size);
+
+int main() {
+
+    int first, last, size, x, i;
+
+    cout<<"Enter the lengh of array: ";
+    cin>>size;
+
+    int arr[size];
+
+    cout<<"Enter elements in ascending order: ";
+    for(i=0; i<size; i++)
+        cin>>arr[i];
+
+    cout<<"Enter the element for search: ";
+    cin>>x;
+
+    cout<<"Array: ";
+    for(i=0; i<size; i++)
+        cout<<arr[i]<<" ";
+
+    first=firstOccurrence(arr, x, size);
+    cout<<"\nFirst Occurrence of element at index: "<<first<<endl;
+
+    last=lastOccurrence(arr, x, size);
+    cout<<"Last Occurrence of element at index: "<<last<<endl;
+
+    return 0;
+
+}
+
+int firstOccurrence(int arr[], int x, int size) {
+
+    int low, mid, high, answer;
+
+    low=0;
+    high=size;
+    mid=low+(high-low)/2;
+
+    while(low<=high) {
+
+        if(x==arr[mid]) {
+            answer=mid;
+            high=mid-1;
+        }
+
+        else if(x<arr[mid])
+            high=mid-1;
+
+        else
+            low=mid+1;
+
+        mid=low+(high-low)/2;
+    }
+
+    return answer;
+
+}
+
+int lastOccurrence(int arr[], int x, int size) {
+
+    int low, mid, high, answer;
+
+    low=0;
+    high=size;
+    mid=low+(high-low)/2;
+
+    while(low<=high) {
+
+        if(x==arr[mid]) {
+            answer=mid;
+            low=mid+1;
+        }
+
+        else if(x<arr[mid])
+            high=mid-1;
+
+        else
+            low=mid+1;
+
+        mid=low+(high-low)/2;
+    }
+
+    return answer;
+    
+}
