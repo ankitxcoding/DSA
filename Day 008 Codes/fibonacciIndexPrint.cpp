4c99d7b7a373e746621fb17c46a97a35433b3306
ankitxcoding <ankitxcoding@gmail.com>
2022-11-25 20:43:23 +0530

    fibonacciIndexPrint Program

--- Day 008 Codes/fibonacciIndexPrint.cpp
+++ Day 008 Codes/fibonacciIndexPrint.cpp
@@ -1,28 +1,62 @@
 #include<iostream>
 using namespace std;
 
-void fibonnaci();
+void fibonnaciPrint();
+void fibonnaciIndex();
 
 int main() {
 
-    fibonnaci();
+    fibonnaciPrint();
+    fibonnaciIndex();
 
 }
 
-void fibonnaci() {
+void fibonnaciPrint() {
 
-    int n, i, x=0, y=1;
+    int n, temp, x=0, y=1;
 
-    cout<<"Enter a number to find fibonacci sequence till that number: ";
+    cout<<"Enter the length of fibonacci series: ";
     cin>>n;
 
     cout<<x<<" "<<y<<" ";
 
-    for(i=0; i<n-i/2; i++) {
-        i=x+y;
+    for(int i=0; i<n-2; i++) {
+        temp=x+y;
         x=y;
-        y=i;
-        cout<<i<<" ";
+        y=temp;
+        cout<<y<<" ";
     }
 
 }
+
+void fibonnaciIndex() {
+
+    int temp, x=0, y=1, index;
+
+    cout<<"\nEnter the index: ";
+    cin>>index;
+
+    for(int i=0; i<index-2; i++) {
+        temp=x+y;
+        x=y;
+        y=temp;
+    }
+
+    if(y==1)
+        cout<<"The element at index "<<index<<" is "<<x;
+    else
+        cout<<"The element at index "<<index<<" is "<<y;
+
+}
+
+// Output-
+
+// Enter the length of fibonacci series: 10
+// 0 1 1 2 3 5 8 13 21 34 
+// Enter the index: 5
+// The element at index 5 is 3
+
+// Enter the length of fibonacci series: 12
+// 0 1 1 2 3 5 8 13 21 34 55 89 
+// Enter the index: 10
+// The element at index 10 is 34
