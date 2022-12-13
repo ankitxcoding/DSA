55285979b202fef3a84d1359bf0be58420069415
ankitxcoding <ankitxcoding@gmail.com>
2022-12-13 19:18:26 +0530

    stlList Code

--- Day 019 Codes/stlList.cpp
+++ Day 019 Codes/stlList.cpp
@@ -8,10 +8,10 @@ int main() {
 
     int size, element;
 
-    cout<<"Enter the length of array: ";
+    cout<<"Enter the length of list: ";
     cin>>size;
 
-    cout<<"Enter the elements of array: ";
+    cout<<"Enter the elements of list: ";
 
     for(int i=0; i<size; i++) {
         cin>>element;
@@ -26,33 +26,35 @@ int main() {
     cout<<"\nFront: "<<list.front()<<endl;
     cout<<"Back: "<<list.back()<<endl;
 
-    cout<<"The size of deque is: "<<list.size()<<endl;
+    cout<<"The size of list is: "<<list.size()<<endl;
 
-    // cout<<"The capacity of deque is: "<<list.capacity()<<endl; capacity is not applicable in deque
+    // cout<<"The capacity of deque is: "<<list.capacity()<<endl; capacity is not applicable in list
 
     list.push_front(1);
 
-    cout<<"Deque after front push: ";
+    cout<<"List after front push: ";
 
     for(auto &k : list)
         cout<<k<<" ";
 
     list.pop_back();
 
-    cout<<"\nDeque after pop back: ";
+    cout<<"\nList after pop back: ";
 
     for(auto &k : list)
         cout<<k<<" ";
 
     list.pop_front();
 
-    cout<<"\nDeque after pop front: ";
+    cout<<"\nList after pop front: ";
 
     for(auto &k : list)
         cout<<k<<" ";
 
-    list.erase(list.begin()+1, list.begin()+3); //it is the range for erase, element at index 1 & 2 gets deleted
-    cout<<"\nDeque after erase random element: ";
+    list.remove(2);
+    list.remove(3);
+
+    cout<<"\nList after erase random element: ";
 
     for(auto &k : list)
         cout<<k<<" ";
@@ -61,15 +63,13 @@ int main() {
 
 }
 
-// Output-
-
-// Enter the length of array: 5
-// Enter the elements of array: 2 3 4 5 6
+// Enter the length of list: 5
+// Enter the elements of list: 2 3 4 5 6
 // Deque: 2 3 4 5 6 
 // Front: 2
 // Back: 6
-// The size of deque is: 5
-// Deque after front push: 1 2 3 4 5 6 
-// Deque after pop back: 1 2 3 4 5 
-// Deque after pop front: 2 3 4 5 
-// Deque after erase random element: 2 5
+// The size of list is: 5
+// List after front push: 1 2 3 4 5 6 
+// List after pop back: 1 2 3 4 5 
+// List after pop front: 2 3 4 5 
+// List after erase random element: 4 5 
