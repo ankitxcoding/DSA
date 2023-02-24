632e1188e9487c1a19f5d89db27516b919b6cdc2
ankitxcoding <ankitxcoding@gmail.com>
2023-02-24 12:28:13 +0530

    mergeSortedArray program but it not working with vectors

---
+++ Day 020 Codes/mergeSortedArray.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+
+    int n, m, size;
+
+    cout<<"Enter the size of first array: ";
+    cin>>n;
+
+    int arr1[n];
+
+    cout<<"Enter the elements of first array: ";
+    for(int i=0; i<n; i++)
+        cin>>arr1[i];
+
+    cout<<"Enter the size of second array: ";
+    cin>>m;
+
+    int arr2[m];
+
+    cout<<"Enter the elements of first array: ";
+    for(int i=0; i<m; i++)
+        cin>>arr2[i];
+
+    // int arr1[]={1, 3, 5, 7 ,9};
+    // int arr2[]={2, 4, 6, 8};
+
+    n=sizeof(arr1)/sizeof(arr1[0]);
+    m=sizeof(arr2)/sizeof(arr2[0]);
+
+    size=n+m;
+
+    int arr3[size];
+
+    cout<<"First Array: "<<endl;
+    for(int i=0; i<n; i++)
+        cout<<arr1[i]<<" ";
+
+    cout<<endl;
+
+    cout<<"Second Array: "<<endl;
+    for(int i=0; i<m; i++)
+        cout<<arr2[i]<<" ";
+
+    int i=0, j=0, k=0;
+
+    while(i<n && j<m) {
+
+        if(arr1[i]<arr2[j])
+            arr3[k++]=arr1[i++];
+
+        else
+            arr3[k++]=arr2[j++];
+
+    }
+
+    while(i<n)
+        arr3[k++]=arr1[i++];
+
+    while(j<m)
+        arr3[k++]=arr2[j++];
+
+    cout<<"\nSorted Array: ";
+    for(int i=0; i<size; i++)
+        cout<<arr3[i]<<" ";
+}
+
+// Output-
+
+// Enter the size of first array: 5
+// Enter the elements of first array: 1 3 5 7 9
+// Enter the size of second array: 4
+// Enter the elements of first array: 2 4 6 8
+// First Array: 
+// 1 3 5 7 9 
+// Second Array: 
+// 2 4 6 8 
+// Sorted Array: 1 2 3 4 5 6 7 8 9 
