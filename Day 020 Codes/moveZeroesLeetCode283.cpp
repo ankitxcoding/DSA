3ef29cd7ea306bfdf59c76a15d1e0b01e1180125
ankitxcoding <ankitxcoding@gmail.com>
2023-03-14 16:41:02 +0530

    move zeroes leetcode 283

---
+++ Day 020 Codes/moveZeroesLeetCode283.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+
+        int nonZero=0, count=0;
+
+        int size=nums.size();
+
+        for(int i=0; i<size; i++) {
+
+            if(nums[i]!=0) {
+                swap(nums[nonZero], nums[i]);
+                nonZero++;
+            }
+        }
+        
+    }
+};
---
+++ Day 021 Codes/rotateArrayLeetCode.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+
+        int size=nums.size();
+        vector<int> result(size);
+
+        for(int i=0; i<size; i++)
+            result[(i+k)%size]=nums[i];
+
+        nums=result;
+        
+    }
+};
